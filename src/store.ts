1a65da46d5652f61b483fb665236d2e87516dbbf
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-18 10:59:32 +0700

    add redux-saga

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, compose, createStore } from "redux";
 import apiReducer from 'modules/api/reducer';
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  : compose
 
 const reducers = combineReducers({
     api:apiReducer
 })
 
-const store = createStore(reducers)
+const store = createStore(reducers, composeEnhancers())
 
 export default store
